070f775cb031c68f98101af27a5e914d1bc77b28
liuhaixu21 <liuhaixu21@mails.tsinghua.edu.cn>
2023-12-18 23:52:42 +0800

    all  done     except ReadMe.md

--- expToPar_test.cpp
+++ expToPar_test.cpp
@@ -1,22 +1,22 @@
-// // @copyright<lhx>[2023.12.16]
-// #include "expToPar.h"
+// @copyright<lhx>[2023.12.16]
+#include "expToPar.h"
 
-// #include <iostream>
+#include <iostream>
 
-// #include "expToTT.h"
-// #include "operators.h"
+#include "expToTT.h"
+#include "operators.h"
 
-// int main() {
-//   std::string str;
-//   std::cin >> str;
+int main() {
+  std::string str;
+  std::cin >> str;
 
-//   std::vector<Propos> propos;
-//   getInfoOfExp(str, propos);
-//   str += '\0';
-//   std::string mp, dp;
-//   expToPar(expToRPN(str), propos, mp, dp);
+  std::vector<Propos> propos;
+  getInfoOfExp(str, propos);
+  str += '\0';
+  std::string mp, dp;
+  expToPar(expToRPN(str), propos, mp, dp);
 
-//   std::cout << "mp: " << mp << std::endl;
-//   std::cout << "dp: " << dp << std::endl;
-//   return 0;
-// }
+  std::cout << "mp: " << mp << std::endl;
+  std::cout << "dp: " << dp << std::endl;
+  return 0;
+}
--- expToTT_test.cpp
+++ expToTT_test.cpp
@@ -1,17 +1,17 @@
-// // @copyright<lhx>[2023.12.15]
-// #include "expToTT.h"
+// @copyright<lhx>[2023.12.15]
+#include "expToTT.h"
 
-// #include <iostream>
-// #include <string>
+#include <iostream>
+#include <string>
 
-// #include "operators.h"
-// int main() {
-//   std::string str;
-//   std::cin >> str;
-//   std::vector<Propos> propos;
-//   getInfoOfExp(str, propos);
-//   str += '\0';
-//   expToTT(expToRPN(str), propos);
+#include "operators.h"
+int main() {
+  std::string str;
+  std::cin >> str;
+  std::vector<Propos> propos;
+  getInfoOfExp(str, propos);
+  str += '\0';
+  expToTT(expToRPN(str), propos);
 
-//   return 0;
-// }
+  return 0;
+}
